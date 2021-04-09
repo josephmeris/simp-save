64fefb557d90ca59981f015146cf61039dd24a70
Joseph Meris <Josephm@visualrisk.com>
2021-04-09 20:34:57 +0800

    Add User avatar component

---
+++ src/Components/Dashboard/UserAvatar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange } from '@material-ui/core/colors';
+import loyd from '../../Images/loyd.jpg'
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+    orange: {
+        color: theme.palette.getContrastText(deepOrange[500]),
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+      },
+});
+
+class UserAvatar extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <Grid container spacing={2}>
+            <Grid item xs={12}></Grid>
+            <Grid item> 
+                <Avatar className={classes.orange} src={loyd}></Avatar>
+            </Grid>
+            <Grid item xs={3}> John Lloyd Cruz</Grid>
+            </Grid>
+        );
+    }
+}
+
+export default withStyles(styles)(UserAvatar)
--- src/Components/Dashboard/UserMainDashboardComponent.js
+++ src/Components/Dashboard/UserMainDashboardComponent.js
@@ -4,10 +4,9 @@ import GoalsSummary from '../Dashboard/GoalsSummary';
 import UpcomingEvents from '../Dashboard/UpcomingEvents';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from "@material-ui/core/styles";
-import Avatar from '@material-ui/core/Avatar';
 import { deepOrange, deepPurple } from '@material-ui/core/colors';
 import BucketsChart from './BucketsChart';
-import loyd from '../../Images/loyd.jpg'
+import UserAvatar from './UserAvatar';
 
 const styles = theme => ({
     orange: {
@@ -28,8 +27,7 @@ class UserMainDashboardComponent extends Component {
               <Grid item xs = {10}>
                 <Grid container spacing={2}>
                         <Grid item xs={12}></Grid>
-                        <Grid item> <Avatar className={classes.orange} src={loyd}></Avatar></Grid>
-                        <Grid item xs={3}> John Lloyd Cruz</Grid>
+                        <Grid item xs={12}><UserAvatar /></Grid>
                         <Grid item xs={12}> <GoalsSummary /></Grid>
                         <Grid item xs={4}> <UpcomingEvents /></Grid>
                         <Grid item xs={4}> <BucketsChart /></Grid>
