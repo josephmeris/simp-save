fb6c2ff3a0ade4ec78da0d32fd035dee16d7887b
Joseph Meris <Josephm@visualrisk.com>
2021-04-09 19:48:45 +0800

    Add Rechart

--- src/Components/Dashboard/BucketsChart.js
+++ src/Components/Dashboard/BucketsChart.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from "@material-ui/core/styles";
+import Paper from '@material-ui/core/Paper';
+
+const data = [
+    { name: 'Savings', value: 20 },
+    { name: 'Emergency', value: 10 },
+    { name: 'Travel', value: 10 },
+    { name: 'Expenses', value: 60 },
+  ];
+  const COLORS = ['#fdcb6e', '#e17055', '#74b9ff', '#2d3436'];
+
+  const styles = theme => ({
+    titleText: {
+        fontWeight: "fontWeightBold",
+        fontSize: 20
+    },   
+    paper: {
+      padding: theme.spacing(2),
+    }
+});
+
+const renderCustomizedLabel = ({
+    x, y, name, value
+  }) => {
+    return (
+      <text x={x} y={y} fill="#2d3436" textAnchor="end" dominantBaseline="central">
+        {name} ({value})
+      </text>
+    );
+  };
+
 
 class BucketsChart extends Component {
     render() {
         const { classes } = this.props;
         
         return (
-            <div> Chart Area </div>
+            <div> 
+                <Paper elevation={3} className={classes.paper}>
+                    <Typography className={classes.titleText}>My Buckets</Typography>
+                    <PieChart width={800} height={255} onMouseEnter={this.onPieEnter}>
+                        <Pie
+                            data={data}
+                            cx={170}
+                            cy={135}
+                            innerRadius={30}
+                            outerRadius={70}
+                            fill="#8884d8"
+                            paddingAngle={5}
+                            dataKey="value"
+                            label={renderCustomizedLabel}
+                        >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                        </Pie>
+                    </PieChart>
+                </Paper>
+            </div>
         );
     }
 }
 
-export default BucketsChart
+export default withStyles(styles)(BucketsChart)
